84dcc97af0de8e311c9f2ad66801b14342951e71
SID37 <1999login@mail.ru>
2019-11-28 20:10:26 +0300

    sixed show method for Level

--- app/models/level.rb
+++ app/models/level.rb
@@ -11,32 +11,21 @@ class Level
     embeds_many :walls
 
     def show
-        check = []
         r = []
 
         chests.all.each do |c|
-            r << {id: c.id.to_s, x: c.x, y: c.y, name: 'chest'}
-            check << c.x + c.y * width
+            r << {id: c.id.to_s, x: c.x, y: c.y, name: :chest}
         end
 
         monsters.all.each do |c|
-            r << {id: c.id.to_s, x: c.x, y: c.y, name: 'monster'}
-            check << c.x + c.y * width
+            r << {id: c.id.to_s, x: c.x, y: c.y, name: :monster}
         end
 
         walls.all.each do |c|
-            r << {x: c.x, y: c.y, name: 'wall'}
-            check << c.x + c.y * width
+            r << {x: c.x, y: c.y, name: :wall}
         end
 
-        for i in 0..(width - 1)
-            for j in 0..(height - 1)
-                if !check.include?(i + j * width)
-                    r << {x: i, y: j, name: 'floor'}
-                end
-            end
-        end
-        {width: width, height: height, items: r}
+        {width: width, height: height, floor: :floor, items: r}
     end
 
     def self.random_generate
